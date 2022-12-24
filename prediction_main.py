0c9e304e1630630500074d35f7e0f2ccf176eae4
JunwooParkSaribu <junwoo5071@gmail.com>
2022-12-24 20:51:16 +0100

    In development...

--- prediction_main.py
+++ prediction_main.py
@@ -15,6 +15,7 @@ data_path = 'data/1_WT-H2BHalo_noIR/whole cells/20220217_h2b halo_before_irradia
 
 
 if __name__ == '__main__':
+    amplif = 2
     print('python script working dir : ', os.getcwd())
     if len(sys.argv) > 1:
         cur_path = sys.argv[1]
@@ -25,18 +26,19 @@ if __name__ == '__main__':
     print(model_path)
     print(data_path)
 
-    immobile_cutoff = 0.118
     print(f'Loading the data...')
     histones = read_data.read_files(path=data_path)
-    histones_label = make_label.make_label(histones, immobile_cutoff)
+    histones_label = make_label.make_label(histones, radius=0.2, density=0.3)
 
     print(f'Image processing...')
-    histones_imgs, img_size = img_preprocess.preprocessing(histones, img_size=8, amplif=2, channel=1)
-    histones_imgs_2D, img_size = img_preprocess.preprocessing(histones, img_size=8, amplif=2, channel=False)
+    histones_channel, nChannel = img_preprocess.make_channel(histones, immobile_cutoff=0.5, hybrid_cutoff=25)
+    histones_imgs, img_size, time_scale = \
+        img_preprocess.preprocessing(histones, histones_channel, img_size=10, amplif=amplif, channel=nChannel)
+    zoomed_imgs, scaled_size = img_preprocess.zoom(histones_imgs, size=img_size, to_size=(500, 500))
 
     print(f'Reshaping the data...')
-    test_X, test_Y = split_shuffle.split(histones_imgs, histones_label)
-    test_X = test_X.reshape((test_X.shape[0], img_size, img_size, 1))
+    test_X, test_Y = split_shuffle.split(zoomed_imgs, histones_label)
+    test_X = test_X.reshape((test_X.shape[0], scaled_size, scaled_size, 1))
 
     final_model = load_model(model_path)
     final_model.summary()
@@ -45,14 +47,12 @@ if __name__ == '__main__':
         y_predict = np.array([np.argmax(x) for x in final_model.predict(test_X)])
     print('Accuracy = ', np.sum([1 if x == 0 else 0 for x in (test_Y.reshape(-1) - y_predict)])/float(y_predict.shape[0]))
 
-    histone_names = histones_imgs_2D.keys()
-    for i, name in enumerate(histone_names):
-        if i == 50:
-            break
-        sample_img = histones_imgs_2D[name]
-        sample_label = histones_label[name]
-        sample_pred = y_predict[i]
-        zoomed_img, zoom_size = img_preprocess.zoom(sample_img, size=img_size)
-        img_preprocess.img_save(zoomed_img, name,
-                                img_size=zoom_size, label=sample_label, pred=sample_pred, path=cur_path)
+    for i, histone in enumerate(histones):
+        histone_first_pos = [int(histones[histone][0][0] * (10 ** amplif)),
+                             int(histones[histone][0][1] * (10 ** amplif))]
+        channels = histones_channel[histone]
+        if i % 10 == 0:
+            print(f'i={i}')
+        img_preprocess.img_save(zoomed_imgs[histone], histone, scaled_size, label=histones_label[histone],
+                                histone_first_pos=histone_first_pos, amplif=amplif, path='img/pred_imgs/')
 

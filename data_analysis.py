7113573bc804ee64167bf92fad0308e61fbeb728
JunwooParkSaribu <junwoo5071@gmail.com>
2023-01-23 22:01:49 +0100

    In development...

--- data_analysis.py
+++ data_analysis.py
@@ -26,4 +26,4 @@ if __name__ == '__main__':
     histones = DataLoad.read_files(path=data_path, cutoff=params['cut_off'], group_size=params['group_size'])
     #histones = DataLoad.read_file('./data/scratch/1/20220217_h2b halo_cel9_no_ir.rpt_tracked.trxyt', cutoff=params['cut_off'])
 
-    ImagePreprocessor.make_gif(histones, '20220601_H2B Halo_h2b halo_field8_BEFORE', '1886')
+    ImagePreprocessor.make_gif(histones, '20220301_H2B Halo_Field2_no_ir', '500')

2c44c0e88af7f5f057a2f1cdb63bb160d3172647
JunwooParkSaribu <junwoo5071@gmail.com>
2023-02-13 13:31:55 +0100

    In development...

--- DataSave.py
+++ DataSave.py
@@ -9,8 +9,8 @@ def save_report(full_histones, filename='', path=''):
     histone_names = list(histones.keys())
 
     with open(write_file_name, 'w', newline='') as f:
-        fieldnames = ['filename', 'id', 'class_id', 'class_name', 'probability',
-                      'maximum_radius', 'first_x_position', 'first_y_position']
+        fieldnames = ['filename', 'h2b_id', 'predicted_class_id', 'predicted_class_name', 'probability',
+                      'maximum_radius','labeled_class_id', 'first_x_position', 'first_y_position']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
 
@@ -20,20 +20,23 @@ def save_report(full_histones, filename='', path=''):
             first_y_pos = trajectory[0][1]
             file_name = histones[histone_name].get_file_name()
             h2b_id = histones[histone_name].get_id()
-            class_id = histones[histone_name].get_predicted_label()
-            class_name = 'unidentified'
-            if class_id == 0:
-                class_name = 'Immobile'
-            if class_id == 1:
-                class_name = 'Hybrid'
-            if class_id == 2:
-                class_name = 'Mobile'
+            pred_class_id = histones[histone_name].get_predicted_label()
+            manuel_label_id = histones[histone_name].get_manuel_label()
             max_r = histones[histone_name].get_max_radius()
             proba = histones[histone_name].get_predicted_proba()
 
-            writer.writerow({'filename':file_name, 'id':h2b_id, 'class_id':class_id, 'class_name':class_name,
-                             'probability':proba, 'maximum_radius':max_r, 'first_x_position':first_x_pos,
-                             'first_y_position':first_y_pos})
+            pred_class_name = 'unidentified'
+            if pred_class_id == 0:
+                pred_class_name = 'Immobile'
+            if pred_class_id == 1:
+                pred_class_name = 'Hybrid'
+            if pred_class_id == 2:
+                pred_class_name = 'Mobile'
+
+            writer.writerow({'filename':file_name, 'h2b_id':h2b_id, 'predicted_class_id':pred_class_id,
+                             'predicted_class_name':pred_class_name, 'probability':proba, 'maximum_radius':max_r,
+                             'labeled_class_id':manuel_label_id,
+                             'first_x_position':first_x_pos, 'first_y_position':first_y_pos})
             """
             input_string += f'{file_name : <25}\t{trajectory_num : <6}\t{class_num : <4}\t' \
                             f'{class_id : <9}\t{first_x_pos : <9}\t{first_y_pos : <9}'

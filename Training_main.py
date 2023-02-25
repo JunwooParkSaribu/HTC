8c9dedc843b53f9d7ff636ad3fb6f360301faa63
JunwooParkSaribu <junwoo5071@gmail.com>
2023-02-25 23:40:45 +0100

    In development...

--- Training_main.py
+++ Training_main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 data_path = 'data/TrainingSample'
-model_path = 'model5'
+model_path = 'model4'
 report_path = 'result/eval_10500samples_training.trxyt.csv'
 
 

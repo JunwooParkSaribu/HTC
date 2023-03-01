28f5f72479b590b68a15261083b08c261f3e13b7
JunwooParkSaribu <junwoo5071@gmail.com>
2023-03-01 12:34:09 +0100

    In development...

--- scratch.py
+++ scratch.py
@@ -40,15 +40,17 @@ reports = ['./result/pred_wholecells_by_cutoff/cutoff5_model1.csv',
            './result/pred_wholecells_by_cutoff/cutoff5_model7_lab.csv']
 
 
-DataAnalysis.confusion_matrix(reports)
+#DataAnalysis.confusion_matrix(reports)
 #MakeImage.comparison_from_reports(reports, img_save_path='./result/image')
 #MakeImage.make_image_from_single_report('./result/pred_wholecells_by_cutoff/cutoff5_model1.csv', option=0, img_save_path='./result/image')#
 
 
 params = ReadParam.read('.')
+histones = DataLoad.file_distrib(paths=params['data'], cutoff=2, group_size=params['group_size'])  # 16GB RAM
+ImagePreprocessor.make_gif(histones, '20220217_h2b halo_cel9_no_ir.rpt_tracked.trxyt', '1820')
+
 #histones = DataSimulation.make_simulation_data(30)
 #DataSave.save_simulated_data(histones, './data/SimulationData/30_simulated_data.trxyt')
-#histones = DataLoad.file_distrib(paths=['./data/SimulationData/30_simulated_data.trxyt'], cutoff=2, group_size=params['group_size'])[0]  # 16GB RAM
 #ImagePreprocessor.make_channel(histones, immobile_cutoff=3, hybrid_cutoff=8, nChannel=3)
 #histones_imgs, img_size, time_scale = ImagePreprocessor.preprocessing(histones, img_scale=10, amp=2, correction=True)
 #zoomed_imgs, scaled_size = ImagePreprocessor.zoom(histones_imgs, size=img_size, to_size=(500, 500))

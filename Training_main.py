ce546afcaa9e9ec67c8c09c91abdd444ca727485
JunwooParkSaribu <junwoo5071@gmail.com>
2023-03-02 12:33:57 +0100

    updated

--- Training_main.py
+++ Training_main.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
         training_model = ConvModel.HTC()
         training_model.compile(jit_compile=True)
         train_history, test_history = training_model.fit(train_ds, test_ds, epochs=epochs,
-                                                         callback=Callback.EarlyStoppingAtMinLoss(patience=30),
+                                                         callback=Callback.EarlyStoppingAtMinLoss(patience=15),
                                                          trace='training_test_loss')
         training_model.save(model_path)
 

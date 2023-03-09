4dbf544e6878e07135317b4b41cbac4cb1f52628
JunwooParkSaribu <junwoo5071@gmail.com>
2023-03-09 11:43:56 +0100

    In development...

--- Training_main.py
+++ Training_main.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     #DataSave.save_simulated_data(histones, './data/SimulationData/27000_simulated_data.trxyt')
     histones = DataLoad.file_distrib(paths=['./data/SimulationData/30_simulated_data.trxyt'], cutoff=2,
                                      chunk=False)[0]
-    histones = TrajectoryPhy.trjaectory_rotation(histones, 100)
+    histones = TrajectoryPhy.trjaectory_rotation(histones, 120)
 
     print(f'Channel processing...')
     ImagePreprocessor.make_channel(histones, immobile_cutoff=5, hybrid_cutoff=12, nChannel=params['nChannel'])

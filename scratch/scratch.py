2c255d274205da6098422611c1c837e1d71508c4
JunwooParkSaribu <junwoo5071@gmail.com>
2023-03-10 19:17:59 +0100

    --under development

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -47,7 +47,7 @@ reports = ['../result/pred_wholecells_by_cutoff/cutoff5_model1.csv',
 
 
 #DataAnalysis.confusion_matrix(reports)
-#MakeImage.comparison_from_reports(reports, img_save_path='./result/image')
+#MakeImage.comparison_from_reports(reports, data_path='../', img_save_path='../result/image')
 #MakeImage.make_image_from_single_report('./result/pred_wholecells_by_cutoff/cutoff5_model1.csv', option=0, img_save_path='./result/image')#
 
 #params = ReadParam.read('.')

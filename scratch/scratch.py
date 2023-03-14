efb3e35571428288ed5096c8663cbea7b66a7fa1
JunwooParkSaribu <junwoo5071@gmail.com>
2023-03-14 17:01:56 +0100

    under develop

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -44,12 +44,11 @@ params = ReadParam.read('.')
 reports = ['./result/pred_wholecells_by_cutoff/cutoff5_model1.csv',
            './result/pred_wholecells_by_cutoff/cutoff5_model7_lab.csv', ## model 13 is retrained over model7_lab
            './result/pred_wholecells_by_cutoff/cutoff5_model14.csv',  ## retrained over model13
-           './result/pred_wholecells_by_cutoff/cutoff5_model16.csv'  ## retrained over model14
            ]
 
 
-DataAnalysis.confusion_matrix(reports)
-#MakeImage.comparison_from_reports(reports, data_path='.', img_save_path='./result/image')
+#DataAnalysis.confusion_matrix(reports)
+MakeImage.comparison_from_reports(reports, data_path='.', img_save_path='./result/image')
 #MakeImage.make_image_from_single_report('./result/pred_wholecells_by_cutoff/cutoff5_model1.csv', option=0, img_save_path='./result/image')#
 
 #histones = DataLoad.file_distrib(paths=params['data'], cutoff=2, group_size=params['group_size'], chunk=False)[0]  # 16GB RAM

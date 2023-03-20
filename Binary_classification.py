43a1a23ef53f0218021bd84ef4de3b555c5cd130
JunwooParkSaribu <junwoo5071@gmail.com>
2023-03-20 15:41:36 +0100

    --under develop

--- Binary_classification.py
+++ Binary_classification.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         print(f'Training the data...')
         training_model = ConvModel.HTC(end_neurons=2)
         training_model.build(input_shape=(None, gen.get_scaled_size()[0], gen.get_scaled_size()[1], params['nChannel']))
-        training_model.compile(optimizer=ConvModel.tf.keras.optimizers.Adam(learning_rate=1e-7))
+        training_model.compile(optimizer=ConvModel.tf.keras.optimizers.Adam(learning_rate=1e-6))
         history = training_model.fit(train_ds, validation_data=test_ds, epochs=epochs,
                                      callbacks=[Callback.EarlyStoppingAtMinLoss(patience=35)],
                                      trace='test_loss')

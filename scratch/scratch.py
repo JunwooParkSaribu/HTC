cef45f395aa5c58e09b36eaeee71ac0f59bccad1
JunwooParkSaribu <junwoo5071@gmail.com>
2023-03-24 10:40:21 +0100

    --under develop

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -1,11 +1,10 @@
 import os
 import sys
-print(os.getcwd())
+
 root_path = os.getcwd().split('/scratch')[0]
-print(root_path)
 os.chdir(root_path)
 sys.path.append(root_path)
-print(sys.path)
+
 from analysis import DataAnalysis
 from physics import DataSimulation, TrajectoryPhy
 from imageProcessor import ImagePreprocessor, MakeImage

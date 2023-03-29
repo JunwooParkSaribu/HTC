e0a2e61c4ff27f3d78338cc446e02235ea12e0f4
JunwooParkSaribu <junwoo5071@gmail.com>
2023-03-29 14:50:22 +0200

    --develop

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -65,11 +65,11 @@ reports = [#'./result/pred_wholecells_by_cutoff/cutoff5_model7_lab.csv', ## simu
 
 #[print(DataAnalysis.ratio_calcul(report)) for report in reports]
 #DataAnalysis.confusion_matrix(reports)
-MakeImage.comparison_from_reports(reports, data_path='.', img_save_path='/mnt/c/Users/jwoo/Desktop/19_mobile')
+MakeImage.comparison_from_reports(reports, data_path='.', img_save_path='/mnt/c/Users/jwoo/Desktop/19_immobile')
 #MakeImage.make_image_from_single_report('./result/pred_wholecells_by_cutoff/cutoff5_model19.csv', option=0, img_save_path='./result/image')
 
 #histones = DataLoad.file_distrib(paths=params['data'], cutoff=2, group_size=params['group_size'], chunk=False)[0]  # 16GB RAM
-#ImagePreprocessor.make_gif(histones, '20220217_h2b halo_cell10_no_ir.rpt_tracked.trxyt', 2515, correction=True)
+#ImagePreprocessor.make_gif(histones, '20220217_h2b halo_cel8_no_ir.rpt_tracked.trxyt', 1141, correction=True)
 
 #histones = DataSimulation.make_simulation_data(30)
 #DataSave.save_simulated_data(histones, './data/SimulationData/4500_simulated_data.trxyt')

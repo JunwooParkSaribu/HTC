712054b93a91a1ade8bb4a2114b31c4f80634d7b
JunwooParkSaribu <junwoo5071@gmail.com>
2023-04-06 15:52:30 +0200

    updated

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -62,18 +62,19 @@ reports = [#'./result/pred_wholecells_by_cutoff/cutoff5_model7_lab.csv', ## simu
            #'./result/pred_wholecells_by_cutoff/cutoff5_model24.csv',  ## 13 + 17 (3rd replica of model19 with same cnn)
            #'./result/pred_wholecells_by_cutoff/cutoff5_model25.csv',  ## manuel label , 1024
            #'./result/pred_wholecells_by_cutoff/cutoff5_model26.csv',  ## manuel label , 1024
-           './result/pred_wholecells_by_cutoff/cutoff5_model27.csv',  ## manuel label ,
+           #'./result/pred_wholecells_by_cutoff/cutoff5_model27.csv',  ## manuel label ,
+           './scratch/6.mn_tracked.trxyt.csv'
            ]
 
 
-#[print(DataAnalysis.ratio_calcul(report)) for report in reports]
+[print(DataAnalysis.ratio_calcul(report)) for report in reports]
 #DataAnalysis.confusion_matrix(reports)
 #MakeImage.comparison_from_reports(reports, data_path='.', img_save_path='/mnt/c/Users/jwoo/Desktop/25_26_diff_images')
 print("hello")
-MakeImage.make_image_from_single_report('./scratch/tracks.simple.filtered.sos.csv', option=0, img_save_path='./scratch/test_image')
+MakeImage.make_image_from_single_report(reports[0], option=0, img_save_path='./scratch/test_image')
 
 #histones = DataLoad.file_distrib(paths=params['data'], cutoff=2, group_size=params['group_size'], chunk=False)[0]  # 16GB RAM
-#ImagePreprocessor.make_gif(histones, '20220217_h2b halo_cel8_no_ir.rpt_tracked.trxyt', 3230, correction=True)
+#ImagePreprocessor.make_gif(histones, 'tracks.simple.filtered.sos', 240, correction=True)
 
 #histones = DataSimulation.make_simulation_data(30)
 #DataSave.save_simulated_data(histones, './data/SimulationData/4500_simulated_data.trxyt')

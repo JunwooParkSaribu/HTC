07ef096bdaee53e5c243a2509a38be18a18ca2f1
JunwooParkSaribu <junwoo5071@gmail.com>
2023-06-06 14:55:59 +0200

    split histones into two groups

---
+++ postProcessing/splitHistones.py
@@ -0,0 +1,18 @@
+def split_hybrid_from_otehrs(data:dict | list):
+    histones = {}
+    if type(data) is dict:
+        histones = data
+    elif type(data) is list:
+        for f in data:
+            histones |= f
+    else:
+        raise Exception
+
+    hybrids = {}
+    others = {}
+    for h2b in histones:
+        if histones[h2b].get_predicted_label() == 1:
+            hybrids[h2b] = histones[h2b].copy()
+        else:
+            others[h2b] = histones[h2b].copy()
+    return hybrids, others
